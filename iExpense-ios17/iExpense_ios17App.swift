7464f8b7f01cc696234e29c3d1f5e10cb839cd4e
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 13:56:28 +0200

    Initial Commit

---
+++ iExpense-ios17/iExpense_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_ios17App.swift
+//  iExpense-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
